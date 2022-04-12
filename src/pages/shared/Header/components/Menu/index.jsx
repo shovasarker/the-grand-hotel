7c5a054611c90081ed2b422932cde3422a6d9f4b
ShovaSarker <shovasarker01@gmail.com>
2022-04-12 23:25:11 +0600

    In Menu Component positioning bug fixed for medium and higher screen size devices

--- src/pages/shared/Header/components/Menu/index.jsx
+++ src/pages/shared/Header/components/Menu/index.jsx
@@ -9,7 +9,7 @@ const Menu = () => {
   }
   return (
     <div
-      className={`w-full flex flex-col md:flex-row justify-end items-center gap-4 absolute md:static bg-white left-1/2 -translate-x-1/2 ${
+      className={`w-full flex flex-col md:flex-row justify-end items-center gap-4 absolute md:static bg-white left-1/2 -translate-x-1/2 md:translate-x-0 ${
         isOpen ? 'top-20' : '-top-40'
       } transition-all duration-300`}
     >
